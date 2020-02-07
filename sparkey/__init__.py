406ceee0f8394a35c51faeb855d4f18fb82aaff5
Kristofer Karlsson <spkrka@users.noreply.github.com>
2020-02-07 15:03:06 +0100

    Add getAsString, improve documentation

--- sparkey/__init__.py
+++ sparkey/__init__.py
@@ -128,6 +128,12 @@ if str == bytes:
         if type(s) != str and type(s) != future.types.newstr:
             raise SparkeyException(s + " must be a string")
         return s
+    
+    def _to_str(b, name):
+        if type(b) != str:
+            raise SparkeyException(s + " must be a string")
+        return s
+    
 else:
     def _to_bytes(s, name):
         t = type(s)
@@ -137,10 +143,23 @@ else:
             raise SparkeyException(name + " must be a string")
         return s.encode('utf-8')
 
+    def _to_str(b, name):
+        if b is None: return None
+        t = type(b)
+        if t == str:
+            return b
+        if t != bytes:
+            raise SparkeyException(name + " must be bytes")
+        return s.decode('utf-8')
+
 class LogWriter(object):
     def __init__(self, filename, mode='NEW',
                  compression_type=Compression.NONE, compression_block_size=0):
         """Creates or appends a log file.
+        
+        Types of keys and values can be strings or bytes.
+        For Python 2, this is the same thing.
+        For Python 3, strings will be encoded as UTF-8
 
         This is not threadsafe, don't write to the same file from
         multiple threads or processes.
@@ -212,8 +231,10 @@ class LogWriter(object):
     def put(self, key, value):
         """Append the key-value pair to the log.
 
-        @param key: must be a string
-        @param value: must be a string
+        @param key: type must be bytes or string
+        @param value: type must be bytes or string
+        
+        
 
         """
         self._assert_open()
@@ -228,7 +249,7 @@ class LogWriter(object):
     def delete(self, key):
         """Appends a delete operation of key to the log.
 
-        @param key: must be a string
+        @param key: type must be bytes or string
 
         """
         self._assert_open()
@@ -440,14 +461,24 @@ class HashReader(object):
         return self.__contains__(key)
 
     def get(self, key):
-        """Retrieve the value assosiated with the key
+        """Retrieve the value associated with the key
 
-        @param key: must be a string
+        @param key: type must be bytes or string
 
-        @returns: the value associated with the key, or None if the
+        @returns: bytes representing the value associated with the key, or None if the
                   key does not exist.
         """
         return self._iter.get(key)
+    
+    def getAsString(self, key):
+        """Retrieve the value associated with the key
+
+        @param key: type must be bytes or string
+
+        @returns: a string representing the value associated with the key, or None if the
+                  key does not exist.
+        """
+        return _to_str(self.get(key))
 
     def __len__(self):
         return _hash_numentries(self._reader)
@@ -510,9 +541,9 @@ class HashIterator(object):
     def get(self, key):
         """Get the value associated with the key
 
-        @param key: must be a string
+        @param key: type must be bytes or string
 
-        @returns: the value associated with the key, or None if the
+        @returns: bytes representing the value associated with the key, or None if the
                   key does not exist.
 
         """
@@ -539,6 +570,17 @@ class HashIterator(object):
                                    (valuelen, clen.value))
         value = string_buffer.raw
         return value
+    
+    def getAsString(self, key):
+        """Retrieve the value associated with the key
+
+        @param key: type must be bytes or string
+
+        @returns: a string representing the value associated with the key, or None if the
+                  key does not exist.
+        """
+        return _to_str(self.get(key))
+    
 
 
 class HashWriter(object):
@@ -582,9 +624,9 @@ class HashWriter(object):
     def put(self, k, v):
         """Append the key-value pair to the log.
 
-        @param key: must be a string
+        @param key: type must be bytes or string
 
-        @param value: must be a string
+        @param value: type must be bytes or string
 
         """
         self._logwriter.put(k, v)
@@ -596,7 +638,7 @@ class HashWriter(object):
     def delete(self, k):
         """Appends a delete operation of key to the log.
 
-        @param key: must be a string
+        @param key: type must be bytes or string
 
         """
         self._assert_open()
@@ -671,11 +713,24 @@ class HashWriter(object):
 
         Only finds things that were flushed to the hash.
 
-        @param key: must be a string
+        @param key: type must be bytes or string
 
-        @returns: the value associated with the key, or None if the
+        @returns: bytes representing the value associated with the key, or None if the
                   key does not exist in the hash.
 
         """
         self._assert_open()
         return self._init_reader().get(key)
+
+    def getAsString(self, key):
+        """Performs a hash lookup of a key.
+
+        Only finds things that were flushed to the hash.
+
+        @param key: type must be bytes or string
+
+        @returns: a string representing the value associated with the key, or None if the
+                  key does not exist in the hash.
+
+        """
+        return _to_str(self.get(key))
