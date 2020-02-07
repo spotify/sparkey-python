73b41b88432da2f49946c202c01f68af291e55f9
Keith McNeill <kdm@spotify.com>
2020-02-07 14:10:32 -0500

    update copyright

--- sparkey/__init__.py
+++ sparkey/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2012-2013 Spotify AB
+# Copyright 2012-2020 Spotify AB
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
