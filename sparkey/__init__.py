277769e997b78e8130ef0c7ee06af9224afcd406
Kristofer Karlsson <spkrka@users.noreply.github.com>
2020-02-07 15:04:03 +0100

    Update __init__.py

--- sparkey/__init__.py
+++ sparkey/__init__.py
@@ -127,9 +127,10 @@ if str == bytes:
         return s
     
     def _to_str(b, name):
+        if b is None: return None
         if type(b) != str:
             raise SparkeyException(s + " must be a string")
-        return s
+        return b
     
 else:
     def _to_bytes(s, name):
@@ -147,7 +148,7 @@ else:
             return b
         if t != bytes:
             raise SparkeyException(name + " must be bytes")
-        return s.decode('utf-8')
+        return b.decode('utf-8')
 
 class LogWriter(object):
     def __init__(self, filename, mode='NEW',
